b61c98c413fd35656f0271462553203c83ed955c
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-08-19 19:16:39 -0300

    add Either type and classes Left and Right to use functional error handling

---
+++ src/core/either.ts
@@ -0,0 +1,47 @@
+// App flow: UI -> controller -> use-case -> entity -> use-case -> repository -> DB
+// If everything is ok the flow is going RIGHT
+// If something goes wrong it should go LEFT
+
+// Error
+export class Left<L, R> {
+  readonly value: L
+
+  constructor(value: L) {
+    this.value = value
+  }
+
+  isRight(): this is Right<L, R> {
+    return false
+  }
+
+  isLeft(): this is Left<L, R> {
+    return true
+  }
+}
+
+// Success
+export class Right<L, R> {
+  readonly value: R
+
+  constructor(value: R) {
+    this.value = value
+  }
+
+  isRight(): this is Right<L, R> {
+    return true
+  }
+
+  isLeft(): this is Left<L, R> {
+    return false
+  }
+}
+
+export type Either<L, R> = Left<L, R> | Right<L, R>
+
+export const left = <L, R>(value: L): Either<L, R> => {
+  return new Left(value)
+}
+
+export const right = <L, R>(value: R): Either<L, R> => {
+  return new Right(value)
+}
