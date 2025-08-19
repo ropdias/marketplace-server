64ba126726c88dade65d450b96c40c0a1dded79e
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-08-19 18:58:23 -0300

    create UniqueEntityID core entity

---
+++ src/core/entities/unique-entity-id.ts
@@ -0,0 +1,24 @@
+import { randomUUID } from 'node:crypto'
+import { ValueObject } from './value-object'
+
+interface UniqueEntityIDProps {
+  value: string
+}
+
+export class UniqueEntityID extends ValueObject<UniqueEntityIDProps> {
+  constructor(value?: string) {
+    super({ value: value ?? randomUUID() })
+  }
+
+  toString() {
+    return this.props.value
+  }
+
+  toValue() {
+    return this.props.value
+  }
+
+  equals(id: UniqueEntityID): boolean {
+    return id.toValue() === this.props.value
+  }
+}
