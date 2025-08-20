02678ab6730f348826228ac9a32204fbd1f053ad
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-08-20 13:27:26 -0300

    add setters to entity seller

--- src/domain/marketplace/enterprise/entities/seller.ts
+++ src/domain/marketplace/enterprise/entities/seller.ts
@@ -15,20 +15,40 @@ export class Seller extends AggregateRoot<SellerProps> {
     return this.props.name
   }
 
+  set name(name: string) {
+    this.props.name = name
+  }
+
   get phone() {
     return this.props.phone
+  }
+
+  set phone(phone: string) {
+    this.props.phone = phone
   }
 
   get email() {
     return this.props.email
   }
 
+  set email(email: string) {
+    this.props.email = email
+  }
+
   get password() {
     return this.props.password
   }
 
+  set password(password: string) {
+    this.props.password = password
+  }
+
   get avatar() {
     return this.props.avatar
+  }
+
+  set avatar(avatar: Attachment | undefined) {
+    this.props.avatar = avatar
   }
 
   static create(props: SellerProps, id?: UniqueEntityID) {
