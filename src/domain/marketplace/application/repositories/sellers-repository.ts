efbd0b1a3d4a7da412b2629f3461615cb9e186c7
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-08-20 13:29:32 -0300

    add findById and save methods to SellersRepository

--- src/domain/marketplace/application/repositories/sellers-repository.ts
+++ src/domain/marketplace/application/repositories/sellers-repository.ts
@@ -1,7 +1,9 @@
 import { Seller } from '../../enterprise/entities/seller'
 
 export abstract class SellersRepository {
+  abstract findById(id: string): Promise<Seller | null>
   abstract findByEmail(email: string): Promise<Seller | null>
   abstract findByPhone(phone: string): Promise<Seller | null>
   abstract create(seller: Seller): Promise<void>
+  abstract save(seller: Seller): Promise<void>
 }
