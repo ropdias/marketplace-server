e6ea49362d7f3dafb2ed7f8eb70402c783c80c00
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-08-20 02:19:27 -0300

    create PasswordIsDifferentError

---
+++ src/domain/marketplace/application/use-cases/errors/password-is-different-error.ts
@@ -0,0 +1,7 @@
+import { UseCaseError } from '@/core/errors/use-case-error'
+
+export class PasswordIsDifferentError extends Error implements UseCaseError {
+  constructor() {
+    super('Password is different')
+  }
+}
