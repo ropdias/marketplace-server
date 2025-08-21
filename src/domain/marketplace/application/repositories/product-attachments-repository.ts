027a39567464f23be062ae61543edb1f5434024a
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-08-21 01:02:56 -0300

    create repository ProductAttachmentsRepository

---
+++ src/domain/marketplace/application/repositories/product-attachments-repository.ts
@@ -0,0 +1,8 @@
+import { ProductAttachment } from '../../enterprise/entities/product-attachment'
+
+export abstract class ProductAttachmentsRepository {
+  abstract createMany(attachments: ProductAttachment[]): Promise<void>
+  abstract deleteMany(attachments: ProductAttachment[]): Promise<void>
+
+  abstract findManyByProductId(productId: string): Promise<ProductAttachment[]>
+}
