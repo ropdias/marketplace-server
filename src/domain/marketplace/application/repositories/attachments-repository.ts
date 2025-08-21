ab47a65bfceb625c2e82e088034850fe41a48205
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-08-21 00:08:28 -0300

    add findManyByIds method

--- src/domain/marketplace/application/repositories/attachments-repository.ts
+++ src/domain/marketplace/application/repositories/attachments-repository.ts
@@ -2,5 +2,6 @@ import { Attachment } from '../../enterprise/entities/attachment'
 
 export abstract class AttachmentsRepository {
   abstract findById(id: string): Promise<Attachment | null>
+  abstract findManyByIds(ids: string[]): Promise<Attachment[]>
   abstract create(attachment: Attachment): Promise<void>
 }
