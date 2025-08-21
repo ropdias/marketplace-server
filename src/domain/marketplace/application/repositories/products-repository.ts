68a6a08ee4a28a677e1d74d22ca45d67a252280b
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-08-21 01:07:19 -0300

    add save method to ProductsRepository

--- src/domain/marketplace/application/repositories/products-repository.ts
+++ src/domain/marketplace/application/repositories/products-repository.ts
@@ -2,5 +2,6 @@ import { Product } from '../../enterprise/entities/product'
 
 export abstract class ProductsRepository {
   abstract findById(id: string): Promise<Product | null>
+  abstract save(product: Product): Promise<void>
   abstract create(product: Product): Promise<void>
 }
