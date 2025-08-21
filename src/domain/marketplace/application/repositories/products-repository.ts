eeff847c41cdeef0da5957dac1e137e7347938b8
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-08-21 01:03:17 -0300

    add findById method

--- src/domain/marketplace/application/repositories/products-repository.ts
+++ src/domain/marketplace/application/repositories/products-repository.ts
@@ -1,5 +1,6 @@
 import { Product } from '../../enterprise/entities/product'
 
 export abstract class ProductsRepository {
+  abstract findById(id: string): Promise<Product | null>
   abstract create(product: Product): Promise<void>
 }
