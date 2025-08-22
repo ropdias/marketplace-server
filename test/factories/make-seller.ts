28fd9fd1530eac1d8dc7928ca5a8af88778d6e4d
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-08-22 18:50:32 -0300

    create makeseller factory

---
+++ test/factories/make-seller.ts
@@ -0,0 +1,25 @@
+import { faker } from '@faker-js/faker'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import {
+  Seller,
+  SellerProps,
+} from '@/domain/marketplace/enterprise/entities/seller'
+
+export function makeseller(
+  override: Partial<SellerProps> = {},
+  id?: UniqueEntityID,
+) {
+  const seller = Seller.create(
+    {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      phone: faker.phone.number(),
+      password: faker.internet.password(),
+      avatarId: null,
+      ...override, // we will override any props that have been passed to make this entity
+    },
+    id,
+  )
+
+  return seller
+}
