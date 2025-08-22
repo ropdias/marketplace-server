828e1134b67f647cb23a3fec764e53eb09abe684
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-08-22 12:05:21 -0300

    add create method to CategoriesRepository

--- src/domain/marketplace/application/repositories/categories-repository.ts
+++ src/domain/marketplace/application/repositories/categories-repository.ts
@@ -3,4 +3,5 @@ import { Category } from '../../enterprise/entities/category'
 export abstract class CategoriesRepository {
   abstract findById(id: string): Promise<Category | null>
   abstract findAll(): Promise<Category[]>
+  abstract create(category: Category): Promise<void>
 }
