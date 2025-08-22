96e9216c300215331f8f7e69ebb259c5c2fbc3a1
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-08-21 22:56:51 -0300

    add findManyRecent and findManyBySellerId to ProductsRepository

--- src/domain/marketplace/application/repositories/products-repository.ts
+++ src/domain/marketplace/application/repositories/products-repository.ts
@@ -1,7 +1,24 @@
 import { Product } from '../../enterprise/entities/product'
+import { ProductStatus } from '../../enterprise/entities/value-objects/product-status'
+
+export interface FindManyRecentParams {
+  page?: number
+  search?: string
+  status?: ProductStatus
+}
+
+export interface FindManyBySellerIdParams {
+  sellerId: string
+  search?: string
+  status?: ProductStatus
+}
 
 export abstract class ProductsRepository {
   abstract findById(id: string): Promise<Product | null>
+  abstract findManyRecent(params: FindManyRecentParams): Promise<Product[]>
+  abstract findManyBySellerId(
+    params: FindManyBySellerIdParams,
+  ): Promise<Product[]>
   abstract save(product: Product): Promise<void>
   abstract create(product: Product): Promise<void>
 }
