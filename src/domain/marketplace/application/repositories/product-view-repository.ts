06441097c2522517b75fe871470875c458591e85
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-08-22 01:45:23 -0300

    include exists method to ProductViewRepository

--- src/domain/marketplace/application/repositories/product-view-repository.ts
+++ src/domain/marketplace/application/repositories/product-view-repository.ts
@@ -2,4 +2,8 @@ import { ProductView } from '../../enterprise/entities/product-view'
 
 export abstract class ProductViewRepository {
   abstract create(productView: ProductView): Promise<void>
+  abstract exists(props: {
+    productId: string
+    viewerId: string
+  }): Promise<boolean>
 }
