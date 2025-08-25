03da4fcd841f9f327e46f37439a788df8cfbedd3
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-08-25 15:15:24 -0300

    create AuthenticateSellerUseCase tests

---
+++ src/domain/marketplace/application/use-cases/authenticate-seller.spec.ts
@@ -0,0 +1,44 @@
+import { FakeHasher } from 'test/cryptography/fake-hasher'
+import { FakeEncrypter } from 'test/cryptography/fake-encrypter'
+import { InMemorySellersRepository } from 'test/repositories/in-memory-sellers-repository'
+import { AuthenticateSellerUseCase } from './authenticate-seller'
+import { makeSeller } from 'test/factories/make-seller'
+
+let inMemorySellersRepository: InMemorySellersRepository
+let fakeHasher: FakeHasher
+let fakeEncrypter: FakeEncrypter
+
+let sut: AuthenticateSellerUseCase
+
+describe('Authenticate Seller', () => {
+  beforeEach(() => {
+    inMemorySellersRepository = new InMemorySellersRepository()
+    fakeHasher = new FakeHasher()
+    fakeEncrypter = new FakeEncrypter()
+
+    sut = new AuthenticateSellerUseCase(
+      inMemorySellersRepository,
+      fakeHasher,
+      fakeEncrypter,
+    )
+  })
+
+  it('should be able to authenticate a seller', async () => {
+    const seller = makeSeller({
+      email: 'johndoe@example.com',
+      password: await fakeHasher.hash('123456'),
+    })
+
+    await inMemorySellersRepository.create(seller)
+
+    const result = await sut.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(result.isRight()).toBe(true)
+    if (result.isRight()) {
+      expect(result.value.accessToken).toEqual(expect.any(String))
+    }
+  })
+})
