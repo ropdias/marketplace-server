555510e6e2f86952f69fe372af0837f7fde6665c
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-08-25 13:10:35 -0300

    update FetchAllCategoriesUseCase to use CategoryDTO and CategoryMapper

--- src/domain/marketplace/application/use-cases/fetch-all-categories.ts
+++ src/domain/marketplace/application/use-cases/fetch-all-categories.ts
@@ -1,24 +1,26 @@
 import { Either, right } from '@/core/either'
 import { Injectable } from '@nestjs/common'
-import { Category } from '../../enterprise/entities/category'
 import { CategoriesRepository } from '../repositories/categories-repository'
+import { CategoryDTO } from '../dtos/category-dtos'
+import { CategoryMapper } from '../mappers/category-mapper'
 
 type FetchAllCategoriesUseCaseResponse = Either<
   null,
-  {
-    categories: Category[]
-  }
+  { categories: CategoryDTO[] }
 >
 
 @Injectable()
 export class FetchAllCategoriesUseCase {
-  constructor(private categoriesRepository: CategoriesRepository) {}
+  constructor(
+    private categoriesRepository: CategoriesRepository,
+    private categoryMapper: CategoryMapper,
+  ) {}
 
   async execute(): Promise<FetchAllCategoriesUseCaseResponse> {
     const categories = await this.categoriesRepository.findAll()
 
-    return right({
-      categories,
-    })
+    const categoriesDTO = this.categoryMapper.toDTOList(categories)
+
+    return right({ categories: categoriesDTO })
   }
 }
