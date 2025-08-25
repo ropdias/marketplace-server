ea18590851ca964f86e2e2efeeef4a7d8f6164d6
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-08-25 15:14:48 -0300

    create FakeEncrypter

---
+++ test/cryptography/fake-encrypter.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/require-await */
+import { Encrypter } from '@/domain/marketplace/application/cryptography/encrypter'
+
+export class FakeEncrypter implements Encrypter {
+  async encrypt(payload: Record<string, unknown>): Promise<string> {
+    return JSON.stringify(payload)
+  }
+}
