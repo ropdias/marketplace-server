0fc590ce44712d5cb9e5b8551d1198acdda8de2e
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-08-27 16:34:29 -0300

    create CategoryMapper tests

---
+++ src/domain/marketplace/application/mappers/category-mapper.spec.ts
@@ -0,0 +1,41 @@
+import { CategoryMapper } from './category-mapper'
+import { makeCategory } from 'test/factories/make-category'
+
+let sut: CategoryMapper
+
+describe('CategoryMapper', () => {
+  beforeEach(() => {
+    sut = new CategoryMapper()
+  })
+
+  it('should map category to DTO', () => {
+    const category = makeCategory()
+
+    const dto = sut.toDTO(category)
+
+    expect(dto).toEqual({
+      id: category.id.toString(),
+      title: category.title,
+      slug: category.slug.value,
+    })
+  })
+
+  it('should map a category list to a DTO List', () => {
+    const categories = [makeCategory(), makeCategory()]
+
+    const dto = sut.toDTOList(categories)
+
+    expect(dto).toEqual([
+      {
+        id: categories[0].id.toString(),
+        title: categories[0].title,
+        slug: categories[0].slug.value,
+      },
+      {
+        id: categories[1].id.toString(),
+        title: categories[1].title,
+        slug: categories[1].slug.value,
+      },
+    ])
+  })
+})
