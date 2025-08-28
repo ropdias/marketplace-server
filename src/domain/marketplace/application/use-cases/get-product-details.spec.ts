dbd3d63337d6070fb39f57f88156c3b47ba8e50c
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-08-28 20:57:32 -0300

    refactor to use productDetailsMapper

--- src/domain/marketplace/application/use-cases/get-product-details.spec.ts
+++ src/domain/marketplace/application/use-cases/get-product-details.spec.ts
@@ -2,9 +2,7 @@ import { makeSeller } from 'test/factories/make-seller'
 import { InMemorySellersRepository } from 'test/repositories/in-memory-sellers-repository'
 import { SellerProfileFactory } from '../factories/seller-profile-factory'
 import { InMemoryAttachmentsRepository } from 'test/repositories/in-memory-attachments-repository'
-import { makeAttachment } from 'test/factories/make-attachment'
 import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error'
-import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { SellerProfileMapper } from '../mappers/seller-profile-mapper'
 import { AttachmentMapper } from '../mappers/attachment-mapper'
 import { GetProductDetailsUseCase } from './get-product-details'
@@ -16,8 +14,6 @@ import { ProductDetailsMapper } from '../mappers/product-details-mapper'
 import { CategoryMapper } from '../mappers/category-mapper'
 import { makeProduct } from 'test/factories/make-product'
 import { makeCategory } from 'test/factories/make-category'
-import { ProductAttachmentList } from '../../enterprise/entities/product-attachment-list'
-import { ProductAttachment } from '../../enterprise/entities/product-attachment'
 import { SellerProfileAssembler } from '../assemblers/seller-profile-assembler'
 import { ProductDetailsAssembler } from '../assemblers/product-details-assembler'
 
@@ -74,7 +70,7 @@ describe('Get Product Details', () => {
     )
   })
 
-  it('should be able to get product details without owner avatar and attachments', async () => {
+  it('should be able to get product details', async () => {
     const seller = makeSeller()
     await inMemorySellersRepository.create(seller)
 
@@ -91,173 +87,23 @@ describe('Get Product Details', () => {
 
     expect(result.isRight()).toBe(true)
     if (result.isRight()) {
-      expect(result.value.productDetails).toMatchObject({
-        productId: product.id.toString(),
-        title: product.title,
-        description: product.description,
-        priceInCents: product.priceInCents.value,
-        status: product.status.value,
-        owner: {
-          sellerId: seller.id.toString(),
-          name: seller.name,
-          phone: seller.phone,
-          email: seller.email,
-          avatar: null,
-        },
-        category: {
-          id: category.id.toString(),
-          title: category.title,
-          slug: category.slug.value,
-        },
+      const sellerProfile = sellerProfileFactory.create({
+        seller,
+        avatar: null,
+      })
+      const productDetails = productDetailsFactory.create({
+        product,
+        ownerProfile: sellerProfile,
+        category,
         attachments: [],
       })
-    }
-  })
-
-  it('should be able to get product details with owner avatar and without attachments', async () => {
-    const avatar = makeAttachment()
-    await inMemoryAttachmentsRepository.create(avatar)
-    const seller = makeSeller({ avatarId: avatar.id })
-    await inMemorySellersRepository.create(seller)
-
-    const category = makeCategory()
-    await inMemoryCategoriesRepository.create(category)
-
-    const product = makeProduct({
-      ownerId: seller.id,
-      categoryId: category.id,
-    })
-    await inMemoryProductsRepository.create(product)
-
-    const result = await sut.execute({ productId: product.id.toString() })
-
-    expect(result.isRight()).toBe(true)
-    if (result.isRight()) {
-      expect(result.value.productDetails).toMatchObject({
-        productId: product.id.toString(),
-        title: product.title,
-        description: product.description,
-        priceInCents: product.priceInCents.value,
-        status: product.status.value,
-        owner: {
-          sellerId: seller.id.toString(),
-          name: seller.name,
-          phone: seller.phone,
-          email: seller.email,
-          avatar: { id: avatar.id.toString(), url: avatar.url },
-        },
-        category: {
-          id: category.id.toString(),
-          title: category.title,
-          slug: category.slug.value,
-        },
-        attachments: [],
-      })
-    }
-  })
-
-  it('should be able to get product details with owner avatar and attachments', async () => {
-    const avatar = makeAttachment()
-    await inMemoryAttachmentsRepository.create(avatar)
-    const seller = makeSeller({ avatarId: avatar.id })
-    await inMemorySellersRepository.create(seller)
-
-    const category = makeCategory()
-    await inMemoryCategoriesRepository.create(category)
-
-    const attachment1 = makeAttachment()
-    const attachment2 = makeAttachment()
-    await inMemoryAttachmentsRepository.create(attachment1)
-    await inMemoryAttachmentsRepository.create(attachment2)
-
-    const product = makeProduct({
-      ownerId: seller.id,
-      categoryId: category.id,
-    })
-
-    const productAttachmentList = new ProductAttachmentList([
-      ProductAttachment.create({
-        attachmentId: attachment1.id,
-        productId: product.id,
-      }),
-      ProductAttachment.create({
-        attachmentId: attachment2.id,
-        productId: product.id,
-      }),
-    ])
-
-    product.attachments = productAttachmentList
-
-    await inMemoryProductsRepository.create(product)
-
-    const result = await sut.execute({ productId: product.id.toString() })
-
-    expect(result.isRight()).toBe(true)
-    if (result.isRight()) {
-      expect(result.value.productDetails).toMatchObject({
-        productId: product.id.toString(),
-        title: product.title,
-        description: product.description,
-        priceInCents: product.priceInCents.value,
-        status: product.status.value,
-        owner: {
-          sellerId: seller.id.toString(),
-          name: seller.name,
-          phone: seller.phone,
-          email: seller.email,
-          avatar: { id: avatar.id.toString(), url: avatar.url },
-        },
-        category: {
-          id: category.id.toString(),
-          title: category.title,
-          slug: category.slug.value,
-        },
-        attachments: [
-          { id: attachment1.id.toString(), url: attachment1.url },
-          { id: attachment2.id.toString(), url: attachment2.url },
-        ],
-      })
+      const productDetailsDTO = productDetailsMapper.toDTO(productDetails)
+      expect(result.value.productDetails).toMatchObject(productDetailsDTO)
     }
   })
 
   it('should return ResourceNotFoundError if product does not exist', async () => {
     const result = await sut.execute({ productId: 'non-existent-id' })
-
-    expect(result.isLeft()).toBe(true)
-    if (result.isLeft()) {
-      expect(result.value).toBeInstanceOf(ResourceNotFoundError)
-    }
-  })
-
-  it('should return ResourceNotFoundError if seller does not exist', async () => {
-    const category = makeCategory()
-    await inMemoryCategoriesRepository.create(category)
-
-    const product = makeProduct({
-      ownerId: UniqueEntityID.create({ value: 'non-existent-owner-id' }),
-      categoryId: category.id,
-    })
-    await inMemoryProductsRepository.create(product)
-
-    const result = await sut.execute({ productId: product.id.toString() })
-
-    expect(result.isLeft()).toBe(true)
-    if (result.isLeft()) {
-      expect(result.value).toBeInstanceOf(ResourceNotFoundError)
-    }
-  })
-
-  it('should return ResourceNotFoundError if category does not exist', async () => {
-    const seller = makeSeller()
-    await inMemorySellersRepository.create(seller)
-
-    const product = makeProduct({
-      ownerId: seller.id,
-      categoryId: UniqueEntityID.create({ value: 'non-existent-category-id' }),
-    })
-    await inMemoryProductsRepository.create(product)
-
-    const result = await sut.execute({ productId: product.id.toString() })
 
     expect(result.isLeft()).toBe(true)
     if (result.isLeft()) {
