2ab9f6725c2b1ac5e87f99dad561ba6c33db1f29
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-09-08 23:41:11 -0300

    add global guard with JWT strategy using Passport

--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common'
 import { JwtModule } from '@nestjs/jwt'
 import { EnvService } from '../env/env.service'
 import { EnvModule } from '../env/env.module'
+import { PassportModule } from '@nestjs/passport'
+import { APP_GUARD } from '@nestjs/core'
+import { JwtAuthGuard } from './jwt-auth.guard'
+import { JwtStrategy } from './jwt.strategy'
 
 @Module({
   imports: [
+    PassportModule,
     JwtModule.registerAsync({
       global: true,
       imports: [EnvModule],
@@ -21,6 +26,12 @@ import { EnvModule } from '../env/env.module'
       },
     }),
   ],
-  providers: [],
+  providers: [
+    JwtStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AuthModule {}
---
+++ src/infra/auth/jwt-auth.guard.ts
@@ -0,0 +1,25 @@
+import { ExecutionContext, Injectable } from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
+import { AuthGuard } from '@nestjs/passport'
+import { Public } from './public.decorator'
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super()
+  }
+
+  canActivate(context: ExecutionContext) {
+    // ✅ getAllAndOverride checks the method first, then the class
+    const isPublic = this.reflector.getAllAndOverride<boolean>(Public, [
+      context.getHandler(), // Checks if the method has @Public()
+      context.getClass(), // Checks if the class has @Public()
+    ])
+
+    if (isPublic) {
+      return true // Allows access without authentication
+    }
+
+    return super.canActivate(context)
+  }
+}
