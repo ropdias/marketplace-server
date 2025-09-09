0df4c55aa0bb03c3cc4ce5de30375b4a056903a3
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-09-09 18:11:43 -0300

    add PrismaProductAttachmentMapper for converting between PrismaAttachment and domain ProductAttachment

---
+++ src/infra/database/prisma/mappers/prisma-product-attachment-mapper.ts
@@ -0,0 +1,41 @@
+import {
+  Prisma,
+  Attachment as PrismaAttachment,
+} from '@/generated/prisma/client'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { ProductAttachment } from '@/domain/marketplace/enterprise/entities/product-attachment'
+
+export class PrismaProductAttachmentMapper {
+  static toDomain(raw: PrismaAttachment): ProductAttachment {
+    if (!raw.productId) {
+      throw new Error('Invalid attachment type.')
+    }
+
+    return ProductAttachment.create(
+      {
+        productId: UniqueEntityID.create({ value: raw.productId }),
+        attachmentId: UniqueEntityID.create({ value: raw.id }),
+      },
+      UniqueEntityID.create({ value: raw.id }),
+    )
+  }
+
+  static toPrismaUpdateMany(
+    attachments: ProductAttachment[],
+  ): Prisma.AttachmentUpdateManyArgs {
+    const attachmentIds = attachments.map((attachment) => {
+      return attachment.attachmentId.toString()
+    })
+
+    return {
+      where: {
+        id: {
+          in: attachmentIds,
+        },
+      },
+      data: {
+        productId: attachments[0].productId.toString(),
+      },
+    }
+  }
+}
