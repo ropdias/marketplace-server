7c221e6f0053f33a3465a5d84d210f976f3a9793
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-09-13 03:32:24 -0300

    feat: add NODE_ENV configuration to environment schema

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -10,6 +10,9 @@ export const envSchema = z.object({
   JWT_PRIVATE_KEY: z.string(),
   JWT_PUBLIC_KEY: z.string(),
   PORT: z.coerce.number().optional().default(3333),
+  NODE_ENV: z
+    .enum(['development', 'production', 'test'])
+    .default('development'),
 })
 
 export type Env = z.infer<typeof envSchema>
