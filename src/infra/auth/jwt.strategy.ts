e06bb10fca8a0c2507d9acc0a6325bccdb6389d7
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-09-13 03:07:20 -0300

    feat: update JwtStrategy to extract JWT from cookies instead of authorization header

--- src/infra/auth/jwt.strategy.ts
+++ src/infra/auth/jwt.strategy.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common'
 import { PassportStrategy } from '@nestjs/passport'
-import { ExtractJwt, Strategy } from 'passport-jwt'
+import { Strategy } from 'passport-jwt'
 import { z } from 'zod'
 import { EnvService } from '../env/env.service'
+import { Request as ExpressRequest } from 'express'
 
 const tokenPayloadSchema = z.object({
   sub: z.uuid(),
@@ -10,13 +11,20 @@ const tokenPayloadSchema = z.object({
 
 export type UserPayload = z.infer<typeof tokenPayloadSchema>
 
+interface RequestWithCookies extends ExpressRequest {
+  cookies: { [key: string]: string }
+}
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(env: EnvService) {
     const publicKey = env.get('JWT_PUBLIC_KEY')
 
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: (request: RequestWithCookies) => {
+        const token = request.cookies?.access_token
+        return typeof token === 'string' ? token : null
+      },
       secretOrKey: Buffer.from(publicKey, 'base64'),
       algorithms: ['RS256'],
     })
