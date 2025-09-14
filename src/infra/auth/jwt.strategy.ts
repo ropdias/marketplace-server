1a1dbc992636df39b60f385a22edc66d94100683
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-09-14 14:23:00 -0300

    feat: refactor JwtStrategy to use JwtCookieService for JWT extraction from cookies

--- src/infra/auth/jwt.strategy.ts
+++ src/infra/auth/jwt.strategy.ts
@@ -3,7 +3,7 @@ import { PassportStrategy } from '@nestjs/passport'
 import { Strategy } from 'passport-jwt'
 import { z } from 'zod'
 import { EnvService } from '../env/env.service'
-import { Request as ExpressRequest } from 'express'
+import { JwtCookieService } from './jwt-cookie.service'
 
 const tokenPayloadSchema = z.object({
   sub: z.uuid(),
@@ -11,20 +11,13 @@ const tokenPayloadSchema = z.object({
 
 export type UserPayload = z.infer<typeof tokenPayloadSchema>
 
-interface RequestWithCookies extends ExpressRequest {
-  cookies: { [key: string]: string }
-}
-
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(env: EnvService) {
+  constructor(env: EnvService, jwtCookie: JwtCookieService) {
     const publicKey = env.get('JWT_PUBLIC_KEY')
 
     super({
-      jwtFromRequest: (request: RequestWithCookies) => {
-        const token = request.cookies?.access_token
-        return typeof token === 'string' ? token : null
-      },
+      jwtFromRequest: jwtCookie.extractJwtFromCookie(),
       secretOrKey: Buffer.from(publicKey, 'base64'),
       algorithms: ['RS256'],
     })
