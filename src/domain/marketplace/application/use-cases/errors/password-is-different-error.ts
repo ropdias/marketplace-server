b2c3cf73088c70587b624c136c0d08808df9035e
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-09-15 17:28:29 -0300

    fix: correct error message punctuation in PasswordIsDifferentError

--- src/domain/marketplace/application/use-cases/errors/password-is-different-error.ts
+++ src/domain/marketplace/application/use-cases/errors/password-is-different-error.ts
@@ -2,6 +2,6 @@ import { UseCaseError } from '@/core/errors/use-case-error'
 
 export class PasswordIsDifferentError extends Error implements UseCaseError {
   constructor() {
-    super('Password is different')
+    super('Password is different.')
   }
 }
