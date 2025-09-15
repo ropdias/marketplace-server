c9aa88c757c462344a449e7754b6a92e35ea75b6
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-09-15 17:55:26 -0300

    fix: update ResourceNotFoundError constructor to accept a custom message

--- src/core/errors/resource-not-found-error.ts
+++ src/core/errors/resource-not-found-error.ts
@@ -1,7 +1,7 @@
 import { UseCaseError } from '@/core/errors/use-case-error'
 
 export class ResourceNotFoundError extends Error implements UseCaseError {
-  constructor() {
-    super('Resource not found')
+  constructor(message = 'Resource not found.') {
+    super(message)
   }
 }
